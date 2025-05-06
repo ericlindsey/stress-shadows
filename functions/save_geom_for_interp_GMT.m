f3e4e0f8b05915280761a47d4e85d3f147042c8d
Eric Lindsey <eol@unm.edu>
2025-05-06 14:37:20 -0600

    add depth to saved output

--- functions/save_geom_for_interp_GMT.m
+++ functions/save_geom_for_interp_GMT.m
@@ -13,7 +13,7 @@ function save_geom_for_interp_GMT(output_filename,geom,values,lat0,lon0)
     % convert patch centers (unicycle format is meters xyz) to lat/lon
     [plat,plon] = xy_to_latlon_polyconic(geom.xc(:,1)/1e3, geom.xc(:,2)/1e3, lon0, lat0);
     
-    outdat=[plon plat values]; %#ok<NASGU>
+    outdat=[plon plat abs(geom.xc(:,3)/1e3) values]; %#ok<NASGU>
     save(output_filename,'outdat','-ASCII');
 
     disp(['Wrote ' output_filename '. Use gmt surface to interpolate values in geographic coordinates.'])
