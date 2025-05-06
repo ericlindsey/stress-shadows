31cd19dc8e71c9d33559b6c498472846176a1039
Eric Lindsey <eol@unm.edu>
2025-05-06 14:34:43 -0600

    update plot slipmodel function

--- functions/plot_jointinv_slipmodel.m
+++ functions/plot_jointinv_slipmodel.m
@@ -4,20 +4,24 @@ function Mw=plot_jointinv_slipmodel(scenario,ax,quiverScale)
 
     for i=1:length(scenario.sources)
         source = scenario.sources{i};
+        N=length(source.Vpl);
         if isa(source, 'Static_Halfspace_Fault_Source')
-            if length(scenario.userParams.slipComponents) == 1 || (isfield(scenario.userParams, 'rakeOptions') && strcmp(scenario.userParams.rakeOptions, 'rakeFixed'))
-                slipmag = abs(source.modelVector);
+            if length(scenario.userParams.slipComponents) == 1
+                slip = source.modelVector;
+                source.geom.plotPatch(slip); shading flat, hold on
+            elseif  (isfield(scenario.userParams, 'faultOptions') && strcmp(scenario.userParams.faultOptions, 'rakeFixed'))
+                slip = source.modelVector(1:N);
+                source.geom.plotPatch(slip); shading flat, hold on
             else
-                slipmag = sqrt(source.modelVector(1:end/2).^2 +source.modelVector(end/2+1:end).^2);
+                slip1 = source.modelVector(1:N/2);
+                slip2 = source.modelVector(N/2+1:N);
+            
+                subplot(2,1,1)
+                source.geom.plotPatch(slip1); shading flat, hold on
+                subplot(2,1,2)
+                source.geom.plotPatch(slip2); shading flat, hold on
+
             end
-            source.geom.plotPatch(slipmag); shading flat, hold on
-            %source.geom.plotPatch()
-
-            [~,Mw] = get_moment_and_magnitude(source.geom, slipmag);
-            disp(['Fault ' num2str(i) ' magnitude: ' num2str(Mw)]);
-             
-            plot_jointinv_slipvectors(scenario,source,ax,quiverScale/5,'k')
-
         end
        
     end
